e24adc1e9ac4c7d5af0cd102b6b9c731de8645c8
meeniebeanie <minyen.j@gmail.com>
2016-08-09 15:12:32 +0800

    added loader, not really working well

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ $(document).ready(function () {
   var $btn = $('#request'); //request button
   var $translate = $('#translatethis'); // translate
   var $output = $('#output'); // translate
+  var $loader = $('.loader');
   var url = 'https://yoda.p.mashape.com/yoda';
+  $loader.hide();
 
   $btn.on('click', function(){
+    $loader.show();
     console.log('clicked');
     $.ajax({
       //The URL to the API that you intend to consume
@@ -24,15 +27,24 @@ $(document).ready(function () {
           xhr.setRequestHeader('X-Mashape-Key', 'Df2BCDlbldmsho3fNxVf13Tm85RPp1WtTxBjsn6BOf2T6hexVF');
         },
 
-        success: function(data) {
-          // JSON.stringify(data);
-          $output.html(data); //box that yoda will speak
-        }
+        // success: function(data) {
+        //   // JSON.stringify(data);
+        //   $loader.hide();
+        //   $output.html(data); //box that yoda will speak
+        // }
+    }).done(function(data){
+      // JSON.stringify(data);
+      $loader.hide();
+      $output.html(data); //box that yoda will speak
     })
 
-    .fail(function (request, textStatus, errorThrown){
-      $output.html(request.status + " " + textStatus + " " + errorThrown);
-    });
+        .always(function(){
+        $loader.hide();
+        })
+
+        .fail(function (request, textStatus, errorThrown){
+          $output.html(request.status + " " + textStatus + " " + errorThrown);
+        });
 
   });
 
